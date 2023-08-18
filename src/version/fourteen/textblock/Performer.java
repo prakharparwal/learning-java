e143aa939e0b423525630425b9805168b4cfd0e2
prakhar.parwal <prakhar.parwal@gmail.com>
2023-08-18 17:30:10 +0530

    Java14 Textblock

--- src/version/fourteen/textblock/Performer.java
+++ src/version/fourteen/textblock/Performer.java
@@ -6,16 +6,24 @@ public class Performer {
                 Hi this is the text block \
                 feature of java
                 """;
-        System.out.println(test);
+        System.out.println("TEST1###"+test);
 
         String test2 = """
-                Hi this is the text block 
+                Hi this is the "text" block
                 feature of java
                 """;
-        System.out.println(test2);
+        System.out.println("TEST2###: "+test2);
 
         String test3 = """
                 
                 """;
+        System.out.println("TEST3###"+test3+"##DONE");
+
+        String test4 = """
+                Something with single space\sand then we can write further
+                """;
+
+        System.out.println("TEST4###: "+test4);
+
     }
 }
