253c56207c253cf48fdd175715fb6ee825e04477
prakhar.parwal <prakhar.parwal@gmail.com>
2023-08-18 17:31:51 +0530

    Java16 Records improvement

---
+++ src/version/sixteen/records_improvement/Test.java
@@ -0,0 +1,12 @@
+package version.sixteen.records_improvement;
+
+import data.Person;
+
+import java.time.LocalDate;
+
+public class Test {
+
+    class InnerClass {
+        Person p = new Person(101, "TEST", LocalDate.of(1992, 9, 19), 67);
+    }
+}
