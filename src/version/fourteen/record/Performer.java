676aa85714515cdbd393d3d0ed8524186a1539cf
prakhar.parwal <prakhar.parwal@gmail.com>
2023-08-18 17:30:50 +0530

    Java14 Records

--- src/version/fourteen/record/Performer.java
+++ src/version/fourteen/record/Performer.java
@@ -2,9 +2,38 @@ package version.fourteen.record;
 
 public class Performer {
     public static void main(String[] args) {
+
+        //1
         Person p = new Person(101, "Prakhar");
+
+        //2
         System.out.println(p.name());
         System.out.println(p.id());
+
+        //3
+        Person p1 = new Person(101, "Prakhar");
+        System.out.println(p1 == p);
+
+        //4
+        System.out.println("Hashcode of p: "+p.hashCode());
+        System.out.println("Hashcode of p1: "+p1.hashCode());
+
+        //5
         System.out.println(p.toString());
+
+        //6.1
+        Person p3 = new Person(101, null);
+        System.out.println(p3);
+
+
+        //6.2
+     //   Person p4 = new Person("TEST");
+     //   System.out.println(p4);
+
+        //6.3
+
+        System.out.println(Person.SOME_FIELD);
+        System.out.println(Person.someMethod());
+
     }
 }
--- src/version/fourteen/record/Person.java
+++ src/version/fourteen/record/Person.java
@@ -1,4 +1,35 @@
 package version.fourteen.record;
 
+import java.util.Objects;
+
 public record Person(int id, String name) {
+
+   /* public Person(String name) {
+        this(000, name);
+    }
+*/
+  /*  public Person(int id, String name, String test) {
+        this.id = id;
+        this.name = name;
+    }
+
+   public Person {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(name);
+    }*/
+    public static String SOME_FIELD = "SomeField";
+    public static String someMethod() {
+        return "Some method";
+    }
 }
+
+/*
+
+
+
+   */
+
+
+
+  /*
+*/
